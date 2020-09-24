c47d159737725f1355360a9583a47fd947d7cc68
Jawnathan <jonathan@jonathanschimpf.com>
2020-09-24 02:00:17 -0400

    most syntax is taken care of. de-buggin issues. needs formatting within the markdown language to add spacing and <hr> lines to seperate and add styling. prints new README successfully each time.

--- index.js
+++ index.js
@@ -1,16 +1,153 @@
-// array of questions for user
+
+
+// require fs + inquirer packages
+
+
+const fs = require("fs");
+
+
+const inquirer = require("inquirer");
+
+
+const generateMarkdown = require("./utils/generateMarkdown");
+
+
+// array of questions for the user, to be apploed within..
+// the appropriate sections on their readme.md file.
+
 const questions = [
+
+    {
+
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+        default: "You Forgot To Add A Project Title.."
+
+    },
+
+    {
+
+        type: "input",
+        message: "Describe your project. What, why, & how?",
+        name: "description",
+        default: "Placeholder for the Project Description.."
+
+    },
+
+    {
+
+        type: "input",
+        message: "What are the steps required to install your project? (If applicable)..",
+        name: "installation",
+        default: "Placeholder for the Installation Steps.."
+
+    },
+
+    {
+
+        type: "input",
+        message: "Provide your instructions and examples for use.",
+        name: "usage",
+        default: "Placeholder for the Usage Instructions.."
+
+    },
+
+    {
+
+        type: "list",
+        message: "Please choose a license from the list below and a badge will be applied at the top of your README file.",
+        name: "license",
+        default: "MIT",
+        choices: 
+        
+        [
+            "Apache",
+            "MIT",
+            "BSD",
+            "GPL"
+        ]
+
+    },
+
+    {
+
+        type: "input",
+        message: "Add guidelines that inform other developers how they can contribute to your project.",
+        name: "contributing",
+        default: "Placeholder for the Contributions Section.."
+
+    },
+
+    {
+
+        type: "input",
+        message: "Go the extra mile and write some tests for your application. Then provide examples on how to run them here.",
+        name: "tests",
+        default: "Placeholder for the Tests Section.."
+
+    },
+    
+    {
+
+        type: "input",
+        message: "List your collaborators, if any. Third-Party Assets? Tutorials?",
+        name: "credits",
+        default: "Placeholder for the Credits Section.."
+
+    },
+
+    {
+
+        type: "input",
+        message: "Enter your primary email address.",
+        name: "email",
+        default: "youforgot@youremail.com",
+
+    },
+
+    {
+
+        type: "input",
+        message: "Please enter your GitHub username here. Do not include an @ symbol.",
+        name: "githubusername",
+        default: "madrobby/vapor.js"
+    }
+    
 
 ];
 
-// function to write README file
+
+// function t)o write README file
+
 function writeToFile(fileName, data) {
-}
+
+    fs.writeFileSync(fileName, data, err => {
+
+        if (err) {
+            return console.log(err);
+        }
+
+    });
+
+    
+    console.log("Congratulations. You are now the proud owner of a brand new README file.")
+
+};
+
 
 // function to initialize program
+
 function init() {
 
-}
+    inquirer.prompt(questions).then(function (answers) {
+
+        writeToFile("GeneratedREADME.md", generateMarkdown(answers));
+
+    })
+};
+
 
 // function call to initialize program
+
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,57 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} ![License](https://img.shields.io/static/v1?label=${data.license}&message=license&color=brightgreen)
+
+
+## Description: 
+${data.description}
+
+
+## Table of Contents: 
+
+* [Installation](##Installation)
+* [Usage](##Usage)
+* [License](##License)
+* [Contributing](##Contributing)
+* [Tests](##Tests)
+* [Credits](##Credits)
+* [Questions](##Questions)
+
+
+## Installation:
+${data.installation}
+
+
+## Usage:
+${data.usage}
+
+
+## License:
+This project is covered under the ${data.license} license. 
+![License](https://img.shields.io/static/v1?label=${data.license}&message=license&color=brightgreen) 
+
+## Contributing:
+${data.contributing}
+
+
+## Tests:
+${data.tests}
+
+
+## Credits:
+${data.credits}
+
+
+## Questions?
+
+Shoot me an e-mail! => ${data.email}
+
+
+Check out my work on GitHub:
+![GitHub](http://github.com/${data.githubusername})
 
 `;
-}
+
+};
 
 module.exports = generateMarkdown;
